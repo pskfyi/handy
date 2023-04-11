6108713ea5b25be5d1e9cbc73422d40606bf51b4
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-04-11 08:37:11 -0700

    refactor: remove unnecessary async/await

--- object/setNestedEntry.test.ts
+++ object/setNestedEntry.test.ts
@@ -1,16 +1,16 @@
 import { assertEquals } from "../deps/testing.ts";
 import { setNestedEntry } from "./setNestedEntry.ts";
 
-Deno.test("object.setNestedEntry", async () => {
+Deno.test("object.setNestedEntry", () => {
   const s = Symbol("");
   const val = () => {};
 
   assertEquals(
-    await setNestedEntry({}, ["a", 2, s], val),
+    setNestedEntry({}, ["a", 2, s], val),
     { a: { 2: { [s]: val } } },
   );
   assertEquals(
-    await setNestedEntry(
+    setNestedEntry(
       { a: { b: 7 } },
       ["a", "b"],
       5,
