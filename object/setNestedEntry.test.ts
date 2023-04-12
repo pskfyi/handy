5a7046cc71d168eda99f89d3211c2d34cbd258b8
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-04-11 20:12:51 -0700

    refactor: label unlabeled assertions

--- object/setNestedEntry.test.ts
+++ object/setNestedEntry.test.ts
@@ -1,20 +1,23 @@
-import { assertEquals, describe } from "../deps/testing.ts";
+import { assertEquals, describe, it } from "../deps/testing.ts";
 import { setNestedEntry } from "./setNestedEntry.ts";
 
-describe("object.setNestedEntry", () => {
-  const s = Symbol("");
-  const val = () => {};
+const s = Symbol("");
+const val = () => {};
 
-  assertEquals(
-    setNestedEntry({}, ["a", 2, s], val),
-    { a: { 2: { [s]: val } } },
-  );
-  assertEquals(
-    setNestedEntry(
-      { a: { b: 7 } },
-      ["a", "b"],
-      5,
-    ),
-    { a: { b: 5 } },
-  );
+describe("object.setNestedEntry", () => {
+  it("creates intermediate objects", () =>
+    assertEquals(
+      setNestedEntry({}, ["a", 2, s], val),
+      { a: { 2: { [s]: val } } },
+    ));
+
+  it("overwrites existing values", () =>
+    assertEquals(
+      setNestedEntry(
+        { a: { b: 7 } },
+        ["a", "b"],
+        5,
+      ),
+      { a: { b: 5 } },
+    ));
 });
