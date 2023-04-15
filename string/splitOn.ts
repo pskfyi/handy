b47d29460b54f3a4dd26907af63c47e69a31ba96
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-04-15 10:13:39 -0700

    feat(string): init; add `splitOn()` & `splitOnFirst()`

---
+++ string/splitOn.ts
@@ -0,0 +1,39 @@
+/** @example splitOnFirst("--", "foo--bar--baz") // ["foo", "bar--baz"] */
+export function splitOnFirst(sep: string, str: string): [string, string] {
+  if (!str || !sep) return [str, ""];
+
+  const index = str.indexOf(sep);
+
+  if (index === -1) return [str, ""];
+
+  return [str.slice(0, index), str.slice(index + sep.length)];
+}
+
+/** @example splitOn(2, "/", "a/b/c/d") // ["a", "b", "c/d"] */
+export function splitOn(
+  count: number,
+  sep: string,
+  str: string,
+): string[] {
+  if (!count || !str || !sep) return [str];
+
+  const result: string[] = [];
+
+  let i = 0;
+  let start = 0;
+
+  while (i < count) {
+    const index = str.indexOf(sep, start);
+
+    if (index === -1) break;
+
+    result.push(str.slice(start, index));
+
+    start = index + sep.length;
+    i++;
+  }
+
+  result.push(str.slice(start));
+
+  return result;
+}
