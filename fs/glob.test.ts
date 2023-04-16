6f0b1e9c0e7829d4a5463bc4fa81ee09e762b466
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-04-15 21:34:42 -0700

    refactor: mark fixture data as private; centralize path

--- fs/glob.test.ts
+++ fs/glob.test.ts
@@ -1,11 +1,11 @@
 import { resolve } from "../deps/path.ts";
 import { assertEquals, describe, it } from "../deps/testing.ts";
 import { glob } from "./glob.ts";
-import { ROOT_DIR } from "../constants.ts";
+import { FIXTURE_DIR } from "../constants.ts";
 
-const GLOB = resolve(ROOT_DIR, "fixture", "**", "*.ts");
+const GLOB = resolve(FIXTURE_DIR, "**", "*.ts");
 
-const A_DIR = resolve(ROOT_DIR, "fixture", "a");
+const A_DIR = resolve(FIXTURE_DIR, "a");
 const C_DIR = resolve(A_DIR, "b", "c");
 
 const A_FILE = resolve(A_DIR, "findme.ts");
