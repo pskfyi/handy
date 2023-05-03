fe1e7bb981ab1e1d7616b42c00130032255c846d
FailingSquare60 <a.arey89@gmail.com>
2023-05-03 16:35:13 -0700

    fix(fs): handle Windows path separators

--- path/globRoot.test.ts
+++ path/globRoot.test.ts
@@ -1,4 +1,4 @@
-import { resolve } from "../_deps/path.ts";
+import { join, resolve, sep } from "../_deps/path.ts";
 import { assertEquals, describe, it } from "../_deps/testing.ts";
 import { globRoot } from "./globRoot.ts";
 
@@ -9,7 +9,7 @@ describe("path.globRoot", () => {
     assertEquals(globRoot(resolve(".")), PROJECT_ROOT_DIR));
 
   it("finds root of a relative glob pattern", () =>
-    assertEquals(globRoot("foo/**/*.ts"), "foo/"));
+    assertEquals(globRoot(join("foo", "**", "*.ts")), "foo".concat(sep)));
 
   it("returns a non-glob path as-is", () =>
     assertEquals(globRoot(PROJECT_ROOT_DIR), PROJECT_ROOT_DIR));
