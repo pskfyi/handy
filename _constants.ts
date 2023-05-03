a64973036221198c337cb09e4f2a9a52b4648476
FailingSquare60 <a.arey89@gmail.com>
2023-05-03 16:39:31 -0700

    fix(fs): use FileUrl schemes

--- _constants.ts
+++ _constants.ts
@@ -1,4 +1,5 @@
-import { resolve } from "./_deps/path.ts";
+import { dirname, fromFileUrl, resolve } from "./_deps/path.ts";
 
-export const ROOT_DIR = new URL(".", import.meta.url).pathname;
-export const FIXTURE_DIR = resolve(ROOT_DIR, "_test", "fixture");
+export const ROOT_DIR = dirname(fromFileUrl(import.meta.url)); //without leading "\\"   on Windows
+
+export const FIXTURE_DIR = resolve(ROOT_DIR, "_test", "fixture"); // join prevents double "C:\" on Windows
