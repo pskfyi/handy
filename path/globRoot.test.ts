044f180c178c8842a19ea6f7b9aac3baedab37b3
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-05-06 10:07:24 -0700

    test(path/globRoot): avoid use of `path` utils

--- path/globRoot.test.ts
+++ path/globRoot.test.ts
@@ -1,16 +1,13 @@
-import { join, resolve, sep } from "../_deps/path.ts";
 import { assertEquals, describe, it } from "../_deps/testing.ts";
 import { globRoot } from "./globRoot.ts";
 
-const PROJECT_ROOT_DIR = resolve(".");
-
 describe("path.globRoot", () => {
   it("finds root of an absolute glob pattern", () =>
-    assertEquals(globRoot(resolve(".")), PROJECT_ROOT_DIR));
+    assertEquals(globRoot("/foo/bar/*.ts"), "/foo/bar/"));
 
   it("finds root of a relative glob pattern", () =>
-    assertEquals(globRoot(join("foo", "**", "*.ts")), "foo".concat(sep)));
+    assertEquals(globRoot("foo/bar/*.ts"), "foo/bar/"));
 
   it("returns a non-glob path as-is", () =>
-    assertEquals(globRoot(PROJECT_ROOT_DIR), PROJECT_ROOT_DIR));
+    assertEquals(globRoot("/foo/bar/"), "/foo/bar/"));
 });
