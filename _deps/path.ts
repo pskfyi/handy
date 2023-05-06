57fad9c06e329758f23131ef4bbfa2066a5f7d3c
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-05-06 10:51:02 -0700

    feat(path): add `dir()`

--- _deps/path.ts
+++ _deps/path.ts
@@ -9,3 +9,11 @@ export {
   sep,
   toFileUrl,
 } from "https://deno.land/std@0.182.0/path/mod.ts";
+
+export {
+  dirname as posixDirname,
+} from "https://deno.land/std@0.182.0/path/posix.ts";
+
+export {
+  dirname as windowsDirname,
+} from "https://deno.land/std@0.182.0/path/win32.ts";
