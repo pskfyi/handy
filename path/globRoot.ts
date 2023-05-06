73fb93676f91bfbe57373217f322de46ea306ee9
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-05-06 10:13:37 -0700

    fix(path/globRoot): make path separator agnostic

--- path/globRoot.ts
+++ path/globRoot.ts
@@ -1,19 +1,22 @@
-import { isGlob, sep } from "../_deps/path.ts";
+import { isGlob } from "../_deps/path.ts";
 
-/** Given a glob pattern, return its non-glob beginning. */
+/** Given a glob pattern, return its non-glob beginning.
+ *
+ * @example
+ * globRoot("/foo/bar/*.ts") // "/foo/bar/"
+ * globRoot("C:\\\\foo\\bar\\*.ts") // "C:\\\\foo\\bar\\"
+ * globRoot("foo/bar") // "foo/bar"
+ */
 export function globRoot(glob: string) {
   let root = "";
 
-  const parts = glob.split(sep);
-  const lastIndex = String(parts.length - 1);
+  const parts = glob.split(/([\/\\])/);
 
   for (const i in parts) {
     const part = parts[i];
     if (isGlob(part)) return root;
 
     root += part;
-
-    if (i !== lastIndex) root += sep;
   }
 
   return root;
