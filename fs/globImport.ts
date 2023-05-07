c388eed408ccf9f7a879599d3ebbb4cb0f76b29f
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-05-07 16:09:41 -0700

    docs(fs/globImport): codify `ImportLike`; refactor JSDocs

--- fs/globImport.ts
+++ fs/globImport.ts
@@ -1,17 +1,29 @@
 import { glob } from "./glob.ts";
 import { toFileUrl } from "../_deps/path.ts";
 
-/** A map from absolute filepaths to `import()` functions. */
+/** A callback that will open a file and return its contents.
+ *
+ * @example
+ * const fn: ImportLike = () => import("./someModule.ts")
+ * const module = await fn()
+ *
+ * @example
+ * const fn: ImportLike = () =>
+ *   Deno.readTextFile("./someFile.txt")
+ * const module = await fn() */
 // deno-lint-ignore no-explicit-any
-export type Modules = Record<string, () => Promise<any>>;
+export type ImportLike = () => Promise<any>;
 
-/** A map from absolute filepaths to `import()` results. */
+/** A map from absolute filepaths to `import()`-like functions which, when
+ * called, return the associated file's contents. */
+export type Modules = Record<string, ImportLike>;
+
+/** A map from absolute filepaths to the contents of those files. */
 // deno-lint-ignore no-explicit-any
 export type EagerModules = Record<string, any>;
 
-/** Given a full `filePath`, return an import function. */
-// deno-lint-ignore no-explicit-any
-export type ImportFactory = (filePath: string) => () => Promise<any>;
+/** Given a full `filePath`, return an `import()`-like function. */
+export type ImportFactory = (filePath: string) => ImportLike;
 
 export type FileHandler = ImportFactory | {
   [Extension: string]: ImportFactory;
@@ -20,7 +32,7 @@ export type FileHandler = ImportFactory | {
 export class FileHandlerError extends Error {
   constructor(
     public readonly filePath: string,
-    public readonly handlers: Record<string, ImportFactory>,
+    public readonly handlers: Record<string, unknown>,
   ) {
     super(
       "Could not find a file handler for the following file:" +
@@ -38,8 +50,7 @@ export const DEFAULT_FILE_HANDLER: FileHandler = {
 export type GlobImportOptions = {
   /** Return file contents rather than import functions. */
   eager?: boolean;
-  /**
-   * A function that turns a file path into an import function, or an object
+  /** A function that turns a file path into an import function, or an object
    * mapping file extensions to such functions. When a file is found, it will
    * match against the first entry that matches it. Use an empty string as an
    * extension for a fallback handler.
@@ -51,20 +62,19 @@ export type GlobImportOptions = {
    * const fileHandler = (filePath) => () => Deno.readTextFile(filePath);
    *
    * @example
-   * const fileHandlers = {
+   * const fileHandler = {
    *   ".ts": (filePath) => () => import(filePath),
-   *   ".json": (path) => () => Deno.readTextFile(path).then(JSON.parse),
+   *   ".json": (path) => () =>
+   *     Deno.readTextFile(path).then(JSON.parse),
    *   "": (filePath) => () => Deno.readTextFile(filePath), // fallback
-   * }
-   */
+   * } */
   fileHandler?: FileHandler;
 };
 
 function makeImportFunction(
   filePath: string,
   handlers: Record<string, ImportFactory>,
-  // deno-lint-ignore no-explicit-any
-): () => Promise<any> {
+): ImportLike {
   for (const [extension, handler] of Object.entries(handlers)) {
     if (filePath.endsWith(extension)) return handler(filePath);
   }
@@ -72,35 +82,33 @@ function makeImportFunction(
   throw new FileHandlerError(filePath, handlers);
 }
 
-/**
- * Given a glob pattern, returns a mapping from the identified filepaths to
- * imports. Like the `glob` utility, this expects an absolute or relative glob
- * pattern rather than a separate base path.
+/** Given a glob pattern, returns a mapping from the identified filepaths to
+ * `ImportLike` callback functions which will return the contents of the
+ * associated file. Like the `glob` utility, this expects an absolute or
+ * relative glob pattern rather than a separate base path.
  *
- * By default, import functions are returned without being called. When
- * `options.eager` is set to `true`, each import function is called and the
- * *contents* of those files are returned instead.
- *
- * Optionally you can pass a map of `FileHandlers`, which describe how to
- * create an import function for a given file extension. See the example below.
+ * Only handles `.ts` files by default. Optionally you can pass a `FileHandler`
+ * describing how to open file types by extension. See the example below.
  *
  * @example
  * const pattern = path.resolve(".", "**", "*.ts")
  * const modules = await globImport(pattern)
  *
- * for (const [filePath, importFn] of Object.entries(modules)) {
+ * for (const [filePath, importLike] of Object.entries(modules)) {
  *    console.log(`Importing ${filePath}...`)
- *    const module = await importFn()
+ *    const module = await importLike()
  * }
  *
  * @example
  * const pattern = path.resolve(".", "**", "*.json")
- * const eager = true // will call each import function
  * const fileHandler = (filePath) =>
  *     () => Deno.readTextFile(filePath).then(JSON.parse)
  *
- * const jsonData = await globImport(pattern, { eager, fileHandler })
- * console.log(jsonData.someFile.someKey[0].someOtherKey...)
+ * const modules = await globImport(pattern, { fileHandler })
+ * for (const [filePath, importLike] of Object.entries(modules)) {
+ *   console.log(`Importing ${filePath}...`)
+ *   const json = await importLike()
+ * }
  *
  * @example
  * const pattern = path.resolve(".", "**", "*.*")
@@ -109,12 +117,29 @@ function makeImportFunction(
  *   "": (filePath) => () => Deno.readTextFile(filePath), // fallback
  * }
  *
- * const modules = await globImport(pattern, { fileHandler })
- */
+ * const modules = await globImport(pattern, { fileHandler }) */
 export async function globImport(
   globPattern: string,
   options?: { eager?: false; fileHandler?: FileHandler },
 ): Promise<Modules>;
+/** Given a glob pattern, returns a mapping from the identified filepaths to the
+ * contents of those files.
+ *
+ * Optionally you can pass a `FileHandler` describing how to open file types by
+ * extension. See the example below.
+ *
+ * @example
+ * const pattern = path.resolve(".", "**", "*.ts")
+ * const modules = await globImport(pattern, { eager: true })
+ * modules["/path/to/someModule.ts"].someExportedFunction()
+ *
+ * @example
+ * const pattern = path.resolve(".", "**", "*.json")
+ * const fileHandler = (filePath) =>
+ *     () => Deno.readTextFile(filePath).then(JSON.parse)
+ *
+ * const data = await globImport(pattern, { eager: true, fileHandler })
+ * data["/path/to/someFile.json"].someKey[0].someOtherKey */
 export async function globImport(
   globPattern: string,
   options: { eager: true; fileHandler?: FileHandler },
@@ -129,11 +154,11 @@ export async function globImport(
   const entries = await Promise.all(
     filePaths.map(async (filePath) => {
       filePath = toFileUrl(filePath).href;
-      const importFunction = typeof fileHandler === "function"
+      const importLike = typeof fileHandler === "function"
         ? fileHandler(filePath)
         : makeImportFunction(filePath, fileHandler);
 
-      return [filePath, eager ? await importFunction() : importFunction];
+      return [filePath, eager ? await importLike() : importLike];
     }),
   );
 
