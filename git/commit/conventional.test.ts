23b8e6231327294bd0b85c46606bcba2c26cbdda
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-05-28 01:34:12 -0700

    refactor(git): use `parser/combinator` for `commit/conventional`

--- git/commit/conventional.test.ts
+++ git/commit/conventional.test.ts
@@ -4,8 +4,10 @@ import {
   describe,
   it,
 } from "../../_deps/testing.ts";
+import { assertParseResult } from "../../parser/asserts.ts";
 import { dedent } from "../../string/dedent.ts";
 import {
+  conventionalCommit as cc,
   ConventionalDescriptionError,
   ConventionalFooterError,
   ConventionalFormatError,
@@ -18,6 +20,257 @@ import {
 function msg(str: string): string {
   return dedent(str).trim();
 }
+
+describe("language", () => {
+  describe("type", () => {
+    it("parses feats", () =>
+      assertParseResult(cc.type.parse("feat: example"), "feat", ": example"));
+
+    it("parses refactors", () =>
+      assertParseResult(cc.type.parse("refactor"), "refactor"));
+
+    it("throws on invalid types", () =>
+      void assertThrows(() => cc.type.parse(" ")));
+  });
+
+  describe("scope", () => {
+    it("parses scopes", () =>
+      assertParseResult(cc.scope.parse("(foo): example"), "foo", ": example"));
+
+    it("throws on invalid input", () =>
+      void assertThrows(() => cc.scope.parse("foo")));
+  });
+
+  describe("delimiter", () => {
+    it('parses ": "', () =>
+      assertParseResult(
+        cc.delimiter.parse(": example"),
+        [false, ": "],
+        "example",
+      ));
+
+    it('parses "!: "', () =>
+      assertParseResult(
+        cc.delimiter.parse("!: example"),
+        [true, ": "],
+        "example",
+      ));
+
+    it("throws on invalid input", () =>
+      void assertThrows(() => cc.delimiter.parse(":")));
+  });
+
+  describe("description", () => {
+    it("parses inline strings", () =>
+      assertParseResult(
+        cc.description.parse("Hello, world!\n\n"),
+        "Hello, world!",
+        "\n\n",
+      ));
+
+    it("throws on invalid input", () =>
+      void assertThrows(() => cc.description.parse("")));
+  });
+
+  describe("footer", () => {
+    it("parses a BREAKING CHANGE", () =>
+      assertParseResult(
+        cc.footer.parse("BREAKING CHANGE: foo\n\nbar\nexclude: this"),
+        { key: "BREAKING CHANGE", value: "foo\n\nbar" },
+        "exclude: this",
+      ));
+
+    it('parses a "#" footer', () =>
+      assertParseResult(
+        cc.footer.parse("Closes #4\ninclude\nexclude: this"),
+        { key: "Closes", value: "#4\ninclude" },
+        "exclude: this",
+      ));
+
+    it("parses a standard footer", () => {
+      assertParseResult(cc.footer.parse("hyphen-ated: foo\n\n\n"), {
+        key: "hyphen-ated",
+        value: "foo",
+      });
+
+      assertParseResult(
+        cc.footer.parse("Reviewed-by: Z"),
+        { key: "Reviewed-by", value: "Z" },
+      );
+
+      assertParseResult(
+        cc.footer.parse("Refs: #123"),
+        { key: "Refs", value: "#123" },
+      );
+    });
+
+    it("throws on invalid input", () => {
+      assertThrows(() => cc.footer.parse(""));
+      assertThrows(() => cc.footer.parse("two words: foo"));
+      assertThrows(() => cc.footer.parse(" starting: space"));
+    });
+  });
+
+  describe("footers", () => {
+    it('parses "#" footers', () =>
+      assertParseResult(
+        cc.footers.parse("Closes #4\nOpens #5"),
+        {
+          footers: [
+            { key: "Closes", value: "#4" },
+            { key: "Opens", value: "#5" },
+          ],
+        },
+      ));
+
+    it("parses standard footers", () =>
+      assertParseResult(
+        cc.footers.parse("Reviewed-by: Z\nRefs: #123"),
+        {
+          footers: [
+            { key: "Reviewed-by", value: "Z" },
+            { key: "Refs", value: "#123" },
+          ],
+        },
+      ));
+
+    it("parses mixed footers", () =>
+      assertParseResult(
+        cc.footers.parse("Closes #4\nReviewed-by: Z"),
+        {
+          footers: [
+            { key: "Closes", value: "#4" },
+            { key: "Reviewed-by", value: "Z" },
+          ],
+        },
+      ));
+  });
+
+  describe("body", () => {
+    it("parses a body", () =>
+      assertParseResult(
+        cc.body.parse("Hello, world!\n\n"),
+        { body: "Hello, world!" },
+      ));
+
+    it("parses a long body", () => {
+      assertParseResult(
+        cc.body.parse(
+          dedent(
+            `Introduce a request id and a reference to latest request. Dismiss
+            incoming responses other than from latest request.
+            
+            Remove timeouts which were used to mitigate the racing issue but are
+            obsolete now.`,
+          ),
+        ),
+        {
+          body: dedent(
+            `Introduce a request id and a reference to latest request. Dismiss
+            incoming responses other than from latest request.
+            
+            Remove timeouts which were used to mitigate the racing issue but are
+            obsolete now.`,
+          ),
+        },
+      );
+    });
+
+    it("parses a body with footers", () =>
+      assertParseResult(
+        cc.body.parse("Hello, world!\n\nCloses #4\n\n"),
+        { body: "Hello, world!" },
+        "Closes #4\n\n",
+      ));
+  });
+
+  describe("message", () => {
+    it("parses one-line messages", () => {
+      assertParseResult(cc.message.parse("feat: description"), {
+        type: "feat",
+        description: "description",
+      });
+
+      assertParseResult(cc.message.parse("fix(md)!: foo"), {
+        type: "fix",
+        scope: "md",
+        description: "foo",
+        breakingChange: true,
+      });
+    });
+
+    it("parses bodies", () => {
+      assertParseResult(cc.message.parse("feat: X\n\nbody"), {
+        type: "feat",
+        description: "X",
+        body: "body",
+      });
+
+      assertParseResult(cc.message.parse("feat: X\n\nbody1\n\nbody2"), {
+        type: "feat",
+        description: "X",
+        body: "body1\n\nbody2",
+      });
+
+      assertParseResult(cc.message.parse("feat: X\n\nbody1\nbody2\n\nbody3"), {
+        type: "feat",
+        description: "X",
+        body: "body1\nbody2\n\nbody3",
+      });
+    });
+
+    it("trims body ends", () => {
+      assertParseResult(cc.message.parse("feat: X\n\nbody1\n\nbody2\n\n"), {
+        type: "feat",
+        description: "X",
+        body: "body1\n\nbody2",
+      });
+    });
+
+    it("parses footers", () => {
+      assertParseResult(cc.message.parse("feat: X\n\nA: B"), {
+        type: "feat",
+        description: "X",
+        footers: [
+          { key: "A", value: "B" },
+        ],
+      });
+
+      assertParseResult(cc.message.parse("feat: X\n\nA: B\nC #1"), {
+        type: "feat",
+        description: "X",
+        footers: [
+          { key: "A", value: "B" },
+          { key: "C", value: "#1" },
+        ],
+      });
+    });
+
+    it("indicates breaking changes", () => {
+      assertParseResult(cc.message.parse("feat: X\n\nBREAKING CHANGE: Y"), {
+        type: "feat",
+        description: "X",
+        breakingChange: true,
+        footers: [{ key: "BREAKING CHANGE", value: "Y" }],
+      });
+
+      assertParseResult(cc.message.parse("feat!: X"), {
+        type: "feat",
+        description: "X",
+        breakingChange: true,
+      });
+    });
+
+    it("throws on invalid messages", () => {
+      assertThrows(() => cc.message.parse(""));
+      assertThrows(() => cc.message.parse("foo"));
+      assertThrows(() => cc.message.parse("feat: "));
+      assertThrows(() => cc.message.parse("feat:! description"));
+      assertThrows(() => cc.message.parse("feat : description"));
+      assertThrows(() => cc.message.parse("feat: description\n\n"));
+    });
+  });
+});
 
 describe("parse", () => {
   describe("official examples", () => {
@@ -186,7 +439,7 @@ describe("parse", () => {
         {
           type: "refactor",
           description: "whatever",
-          footers: [{ key: "this-is-a", value: "mutli\nline\nfooter" }],
+          footers: [{ key: "this-is-a", value: "mutli\n    line\n  footer" }],
         },
       ));
   });
