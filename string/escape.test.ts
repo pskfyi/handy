7f4b8eeefa924a27671f0580384b2ae96b2af235
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-05-27 20:59:14 -0700

    feat(string): add `escape` helpers
    - `escapeFull()` replaces special characters like newline with two-character literals of their escape sequences like `"\n"`
    
    - `escapeTerse()` is converts special characters into single-character unicode symbols, to preserve string length. Importantly, newline becomes `"¶"` and tab becomes `"⇥"`

---
+++ string/escape.test.ts
@@ -0,0 +1,34 @@
+import { assertEquals, describe, it } from "../_deps/testing.ts";
+import { escapeFull, escapeTerse } from "./escape.ts";
+
+describe("escapeFull", () => {
+  it("escapes newlines", () => {
+    assertEquals(escapeFull("\n\n"), "\\n\\n");
+    assertEquals(escapeFull("\r\r"), "\\r\\r");
+    assertEquals(escapeFull("\r\n\r\n"), "\\r\\n\\r\\n");
+  });
+
+  it("escapes tabs", () => assertEquals(escapeFull("\t\t"), "\\t\\t"));
+
+  it("escapes vertical tabs", () => assertEquals(escapeFull("\v\v"), "\\v\\v"));
+
+  it("escapes form feeds", () => assertEquals(escapeFull("\f\f"), "\\f\\f"));
+
+  it("escapes backspaces", () => assertEquals(escapeFull("\b\b"), "\\b\\b"));
+});
+
+describe("escapeTerse", () => {
+  it("escapes newlines", () => {
+    assertEquals(escapeTerse("\n\n"), "¶¶");
+    assertEquals(escapeTerse("\r\r"), "␍␍");
+    assertEquals(escapeTerse("\r\n\r\n"), "␍¶␍¶");
+  });
+
+  it("escapes tabs", () => assertEquals(escapeTerse("\t\t"), "⇥⇥"));
+
+  it("escapes vertical tabs", () => assertEquals(escapeTerse("\v\v"), "␋␋"));
+
+  it("escapes form feeds", () => assertEquals(escapeTerse("\f\f"), "␌␌"));
+
+  it("escapes backspaces", () => assertEquals(escapeTerse("\b\b"), "␈␈"));
+});
