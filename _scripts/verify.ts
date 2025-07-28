51103e7fd83cc524d34a25283f6174431194e3d3
pskfyi <patrick.keenan.fyi@gmail.com>
2025-07-27 17:36:20 -0700

    feat(repo): assert coverage threshold; closes #8

--- _scripts/verify.ts
+++ _scripts/verify.ts
@@ -3,7 +3,7 @@ import { cmds } from "../cli/cmds.ts";
 await cmds([
   "deno fmt",
   "deno lint",
-  "deno test -A",
+  "deno task test-coverage",
   "deno task test-readme",
   "deno task update-exports",
   "deno task update-readme",
